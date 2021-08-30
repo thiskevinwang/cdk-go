f1da8f6e634232ef3b159e05ce116c3117caef88
Kevin Wang <kwangsan@gmail.com>
2021-08-29 22:14:23 -0400

    chore: update lambda argument name

--- lambda/main.go
+++ lambda/main.go
@@ -11,8 +11,8 @@ type MyEvent struct {
 	Name string `json:"name"`
 }
 
-func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-	return fmt.Sprintf("Hello %s!", name.Name), nil
+func HandleRequest(ctx context.Context, event MyEvent) (string, error) {
+	return fmt.Sprintf("Hello %s!", event.Name), nil
 }
 
 func main() {
