36014da2410fc19e55f124da9ddaf73de232540d
Kevin Wang <kwangsan@gmail.com>
2021-09-06 11:57:47 -0400

    feat: add aws sdk to lambda

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.17
 
 require (
 	github.com/aws/aws-cdk-go/awscdk v1.121.0-devpreview
+	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.2.0
 	github.com/aws/constructs-go/constructs/v3 v3.3.141
 	github.com/aws/jsii-runtime-go v1.34.0
 	github.com/stretchr/testify v1.7.0
@@ -15,9 +16,27 @@ require (
 require (
 	github.com/Masterminds/semver/v3 v3.1.1 // indirect
 	github.com/aws/aws-lambda-go v1.26.0
+	github.com/aws/aws-sdk-go-v2/config v1.8.0
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.5.0
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/tidwall/match v1.0.3 // indirect
 	github.com/tidwall/pretty v1.1.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.9.0 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.4.0 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.5.0 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.0.4 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.2.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/dynamodbstreams v1.4.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.3.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.1.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.3.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.4.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.7.0 // indirect
+	github.com/aws/smithy-go v1.8.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 )
--- lambda/main.go
+++ lambda/main.go
@@ -3,20 +3,70 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+
+	// https://github.com/aws/aws-sdk-go-v2/issues/1018#issuecomment-755433782
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/jsii-runtime-go"
 )
 
 type MyEvent struct {
-	Name string `json:"name"`
+	Pk string `json:"pk"`
+	Sk string `json:"sk"`
+}
+
+type MyItem struct {
+	Pk   string `json:"pk"`
+	Sk   string `json:"sk"`
+	Test string `json:"test,omitempty"`
 }
 
 func HandleRequest(ctx context.Context, event MyEvent) (string, error) {
-	// https://gobyexample.com/environment-variables
-	fmt.Println("tablename:", os.Getenv("tablename"))
-	fmt.Println("lettuce:", os.Getenv("lettuce"))
-	return fmt.Sprintf("Hello %s!", event.Name), nil
+	// Using the SDK's default configuration, loading additional config
+	// and credentials values from the environment variables, shared
+	// credentials, and shared configuration files
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion("us-east-1"))
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	// Using the Config value, create the DynamoDB client
+	svc := dynamodb.NewFromConfig(cfg)
+
+	tableName := jsii.String(os.Getenv("tablename"))
+
+	getItemOutput, getItemError := svc.GetItem(ctx, &dynamodb.GetItemInput{
+		TableName: tableName,
+		Key: map[string]types.AttributeValue{
+			"pk": &types.AttributeValueMemberS{Value: event.Pk},
+			"sk": &types.AttributeValueMemberS{Value: event.Sk},
+		},
+	})
+	if getItemError != nil {
+		return fmt.Sprintf("getItemError: %s!", getItemError), nil
+	}
+
+	// "unmarshallErr unmarshal failed, cannot unmarshal to non-pointer value, got main.MyItem!"
+	// var item MyItem
+
+	// "unmarshallErr unmarshal failed, cannot unmarshal to nil value, *main.MyItem!"
+	// var item *MyItem
+
+	item := &MyItem{}
+
+	unmarshallErr := attributevalue.UnmarshalMap(getItemOutput.Item, item)
+	if unmarshallErr != nil {
+		return fmt.Sprintf("unmarshallErr %s!", unmarshallErr), nil
+	}
+	return fmt.Sprintf("Hello %s!", item.Pk), nil
+	// "Hello &{value1 {}}!"
+
 }
 
 func main() {
